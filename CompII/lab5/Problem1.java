3fab2e5a392bd853bff77c05400371ca651bf303
Wyatt Rose <wyatt.r.rose@gmail.com>
2022-09-26 23:45:31 -0400

    complete lab5

--- CompII/lab5/Problem1.java
+++ CompII/lab5/Problem1.java
@@ -1,5 +1,97 @@
 package labs.CompII.lab5;
 
+import java.util.Scanner;
+
 public class Problem1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.printf("input s1, s2, s3: ");
+
+        Triangle t = new Triangle(input.nextDouble(),input.nextDouble(),input.nextDouble());
+
+        System.out.printf("input color: ");
+
+        t.setColor(input.next());
+
+        System.out.printf("input isFilled: ");
+
+        t.setFilled(input.nextBoolean());
+
+        System.out.println("\n"+t.toString());
+
+    }
+}
+
+abstract class GeometricObject {
+    private String color = "white";
+    private boolean filled;
+
+    protected GeometricObject() {
+
+    }
+
+    protected GeometricObject(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() { return color; }
+
+    public void setColor(String color) { this.color = color; }
+
+    public boolean isFilled() { return filled; }
     
+    public void setFilled(boolean filled) { this.filled = filled; }
+
+    public abstract double getArea();
+
+    public abstract double getPerimeter();
+}
+
+class Triangle extends GeometricObject {
+
+    private double side1;
+    private double side2;
+    private double side3;
+
+    public double getSide1() { return side1; }
+    public double getSide2() { return side2; }
+    public double getSide3() { return side3; }
+
+    public Triangle() {
+        this.side1 = 20;
+        this.side2 = 20;
+        this.side3 = 20;
+    }
+
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    @Override
+    public double getArea() {
+        double semi = (side1+side2+side3)/2;
+        return Math.sqrt(semi*(semi-side1)*(semi-side2)*(semi-side3));
+    }
+
+    @Override
+    public double getPerimeter() {
+        return side1+side2+side3;
+    }
+
+    @Override
+    public String toString() {
+        return
+        "Color: "+ getColor() +
+        "\nFilled: "+ isFilled() +
+        "\nArea: "+ getArea() +
+        "\nPermiter: "+ getPerimeter() + 
+        "\nSide1: "+ this.side1 +
+        "\nSide2: "+ this.side2 +
+        "\nSide3: "+ this.side3 +
+        "\n";
+    }
 }
---
+++ CompII/lab5/Problem2.java
@@ -0,0 +1,51 @@
+package labs.CompII.lab5;
+
+import java.util.*;
+
+public class Problem2 {
+
+    /*
+     * Write a test program that prompts the user to enter unlimited number of
+     * integers to a list (end with a character 'q' to finish the input) and
+     * displays the distinct itnegers separated by exactly one space.
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+
+        System.out.printf("Enter integers (separated by spaces, end with character q): ");
+
+        while(true) {
+            String input = in.next();
+
+            if(input.equals("q")) { break; } // if q is input
+
+            arr.add(Integer.parseInt(input)); // add value to the array
+        }
+        
+        removeIdenticalNumbers(arr);
+
+        System.out.printf("Modfied array: ");
+        for(int i : arr) {
+            System.out.printf(" %s",i);
+        }
+    }
+
+    /*
+     * Assume the user can input a list of numbers. Please write a method that
+     * removes the duplicate elements from an array list of integers.
+     */
+
+    public static void removeIdenticalNumbers(ArrayList<Integer> list) {
+
+        // who cares about optimization! this is a small program a linear search is fine
+        for(int i = 0; i < list.size(); i++) {
+            for(int j = 0; j < list.size(); j++) {
+                if((list.get(i) == list.get(j)) && j != i) {
+                    list.remove(j);
+                }
+            }
+        }
+    }
+}
