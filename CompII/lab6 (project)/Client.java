7f0c37a92920b5dd5d2e9ef0660e17101e6aef51
Wyatt Rose <wyatt.r.rose@gmail.com>
2022-09-28 22:35:13 -0400

    modified lab6

--- CompII/lab6 (project)/Client.java
+++ CompII/lab6 (project)/Client.java
@@ -1,40 +1,27 @@
-import java.util.Scanner;
+import java.util.Scanner; // visibility of 
 
-public class Client {
+public class Client { // ( ⓛ ω ⓛ *) *MEOW* start of class!
 
-    // sum
+    public static void main(String[] args) { // maybe name 'args' something different next time...
 
-    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); // initialize the scanner for user input! 
+        double[] values = new double[5]; // create a new array! (the program specified five values...)
 
-        // initialize values
-        Scanner input = new Scanner(System.in);
-        double[] values = new double[5];
+        System.out.printf("Input five values: "); // prompt the user for input (otherwise how will they know?)
+        for(int i = 0; i < values.length; i++) { values[i] = input.nextDouble(); } // get the values from the user!
 
-        // get user input
-        System.out.printf("Input five values: ");
-        for(int i = 0; i < values.length; i++) { values[i] = input.nextDouble(); }
+        double sum = 0; // initialize a new variable! (this one will hold the sum of all elements in the array)
+        for(double i : values) { sum += i; } // calculate the sum of all values in the array!
+        double mean = sum/values.length; // calculate the mean by dividing the sum by the amount of values
 
-        // perform calcuations
+        double temp = 0; // this temporary variable is used to store the summation used in the deviation equation
+        for(double i : values) { // for-each loop! these let you iterate through objects that implement a specific interface! (I forgot the name of it though)
+            temp += (i-mean)*(i-mean); // equation used to calculate a portion of the deviation equation
+        } // end of the for each loop! (I could splice it into one line but it looks nice over three lines...)
+        double deviation = Math.sqrt(temp/values.length); // taking the square root using the Math library, also remainding part of the equation!
 
-        // mean
-        double sum = 0;
-        for(double i : values) { sum += i; }
-        double mean = sum/values.length;
+        System.out.printf("mean:\t\t%.2f%ndeviation:\t%.2f%n",mean,deviation); // display the values of the variables!
 
-        // standard deviation
-
-        double temp = 0;
-        for(double i : values) {
-            temp += (i-mean)*(i-mean);
-        }
-        double deviation = Math.sqrt(temp/values.length);
-
-        // display output
-
-        System.out.printf("mean:\t%d%ndeviation:\t%d%n",mean,deviation);
-
-        input.close();
+        input.close(); // don't forget to close the scanner! (doesn't really matter here though... since the program terminates next line!)
     }
-    // mean
-    // standard deviation
-}
+} // ᓚᘏᗢ *meow* <end of class!>
